3d565e415181ae3a7489aeb6f2283ea9dfa22a15
Israfil Hossen <sisrafilss@gmail.com>
2025-06-21 21:51:24 +0600

    get single book api added

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -59,3 +59,33 @@ bookRouter.get("/", async (req: Request, res: Response) => {
     });
   }
 });
+
+bookRouter.get("/:bookId", async (req: Request, res: Response) => {
+  try {
+    const bookId = req.params.bookId;
+
+    const books = await Book.find({ _id: bookId });
+
+    if (books.length) {
+      res.status(200).json({
+        success: true,
+        message: "Books retrieved successfully",
+        data: books[0],
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "No matching results found",
+        data: {},
+      });
+    }
+  } catch (error: any) {
+    console.log("ERROR", error);
+
+    res.status(400).json({
+      message: error.message,
+      success: false,
+      error,
+    });
+  }
+});
