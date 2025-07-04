d51e6bc6553d403243e424e5981c65682ab54730
Israfil Hossen <sisrafilss@gmail.com>
2025-07-04 08:51:32 +0600

    live url added in cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
+import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import { bookRouter } from "./controllers/book.controller";
 import { borrowRouter } from "./controllers/borrow.controller";
 
 const app: Application = express();
 
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://library-management-client-beta.vercel.app",
+    ],
+  })
+);
 app.use(express.json());
 app.use("/api/books", bookRouter);
 app.use("/api/borrow", borrowRouter);
